cc3e7c8eb2a05ddac5f9d4b646a846035638c65f
m4rio.dev <oiram.araujo@gmail.com>
2020-07-31 13:23:01 -0300

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-174182609-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
